73d6ab364daa8f1596c3a106c73ba14e9294d482
kaesar <kaesar.andres@gmail.com>
2023-09-13 07:49:59 -0500

    Introduce to cicd files

---
+++ iac/cdk-app-runner.ts
@@ -0,0 +1,76 @@
+import * as cdk from '@aws-cdk/core';
+import { CfnService } from '@aws-cdk/aws-apprunner';
+import { Effect, PolicyDocument, PoliciyStatement, Role, ServicePrincipal } from '@aws-cdk/aws-iam'; 
+
+const config = {
+    stage: 'dev',
+    backend: {
+        apprunner_connection_arn: 'GitHub Repo Connection ARN',
+        repository_url: 'https://github.com/kaesar/cicddemo'
+    }
+};
+
+interface AppRunnerStackProps extends cdk.StackProps {
+    readonly stage: string;    
+}
+
+export class CdkStack extends cdk.Stack {
+
+    constructor(scope: cdk.App, id: string, props?: AppRunnerStackProps) {
+        super(scope, id, props);
+
+        const giveReadAccessPolity = new PolicyDocument({
+            statements: [
+                new PoliciyStatement({
+                    resources: ['arn'],
+                    actions: ['secretsmanager:GetSecretValue'],
+                    effect: Effect.ALLOW
+                })
+            ]
+        });
+
+        const instanceRole = new Role(this, `${props?.stage}-AppRunnerRole`, {
+            description: 'Default role for app runner',
+            assumeBy: new ServicePrincipal('tasks.apprunner.amazonaws.com'),
+            inlinePolicies: {
+                GiveReadAccess: giveReadAccessPolity
+            }
+        });
+
+        const appRunnerService = new CfnService(this, `${props?.stage}-AppRunnerService`, {
+            serviceName: `${props?.stage}-AppRunnerService`,
+            sourceConfiguration: {
+                authenticationConfiguration: {
+                    connectionArn: config.backend.apprunner_connection_arn
+                },
+                autoDeploymentsEnabled: false,
+                codeRepository: {
+                    repositoryUrl: config.backend.repository_url,
+                    sourceCodeVersion: {
+                        type: 'BRANCH',
+                        value: 'main'
+                    },
+                    codeConfiguration: {
+                        /*codeConfigurationValues: {
+                            startCommand: 'node dist/index.js',
+                            buildCommand: 'npm install',
+                            port: '3000',
+                            runtime: 'NODEJS_16',
+                            //runtimeEnvironmentSecrets: [{ name: '', value: '' }],
+                            //runtimeEnvironmentVariables: [{ name: '', value: '' }],
+                        },*/
+                        configurationSource: 'API'  // 'REPOSITORY
+                    },
+                }
+            },
+            healthCheckConfiguration: {
+                path: '/health'
+            },
+            instanceConfiguration: {
+                instanceRoleArn: instanceRole.roleArn
+            }
+        });
+
+    }
+
+}
--- iac/main.tf
+++ iac/main.tf
@@ -2,11 +2,79 @@ provider "aws" {
   region = "us-east-1"  # Reemplaza con tu región deseada
 }
 
+resource "aws_iam_policy" "give_read_access_policy" {
+  name        = "GiveReadAccessPolicy"
+  description = "Policy to allow read access to secrets manager"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action   = "secretsmanager:GetSecretValue",
+        Effect   = "Allow",
+        Resource = "arn:aws:secretsmanager:us-east-1:123456789012:secret:your-secret-name-*",
+      },
+    ],
+  })
+}
+
+resource "aws_iam_role" "apprunner_role" {
+  name = "AppRunnerRole"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "tasks.apprunner.amazonaws.com",
+        },
+      },
+    ],
+  })
+
+  inline_policy {
+    name        = "GiveReadAccess"
+    policy_json = aws_iam_policy.give_read_access_policy.json
+  }
+}
+
 resource "aws_apprunner_service" "my_drone_service" {
-  name          = "my-drone-service"
+  service_name = "MyDroneService"
+
+  source_configuration {
+    authentication_configuration {
+      connection_arn = "GitHub Repo Connection ARN"
+    }
+
+    auto_deployments_enabled = false
+
+    code_repository {
+      repository_url = "https://github.com/kaesar/democicd"
+      source_code_version {
+        type  = "BRANCH"
+        value = "main"
+      }
+      code_configuration {
+        configuration_source = "API"  # 'REPOSITORY'
+      }
+    }
+  }
+
+  health_check_configuration {
+    path = "/health"
+  }
+
+  instance_configuration {
+    instance_role_arn = aws_iam_role.apprunner_role.arn
+  }
+}
+
+/*resource "aws_apprunner_service" "my_drone_service" {
+  service_name = "MyDroneService"
   source_configuration {
     image_repository {
       image_identifier = "your-docker-image-url"  # Reemplaza con la URL de tu imagen de Docker
     }
   }
-}
+}*/
